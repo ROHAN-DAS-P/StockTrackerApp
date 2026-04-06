7f1100c6e4f93df3e1d1df94fac342eb1b6d4652
ROHAN DAS P <rohan2003710@gmail.com>
2026-04-06 12:52:36 +0530

    completed thr websocket setup

--- stockproject/stockproject/asgi.py
+++ stockproject/stockproject/asgi.py
@@ -9,8 +9,23 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockproject.settings')
 
-application = get_asgi_application()
+# Needed if starting server using daphne or uvicorn command
+import django
+django.setup()
+
+from channels.auth import AuthMiddlewareStack
+from mainapp.routing import websocket_urlpatterns
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
--- stockproject/stockproject/settings.py
+++ stockproject/stockproject/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'stockproject.wsgi.application'
+ASGI_APPLICATION = 'stockproject.asgi.application'
 
 
 # Database
